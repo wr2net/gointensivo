69065aac22229f9702c3efb91bcf326b8e7bd666
wr2net <wagnerr2@gmail.com>
2023-03-07 22:14:34 -0300

    final first day

--- go.mod
+++ go.mod
@@ -2,11 +2,14 @@ module github.com/wr2net/gointensivo
 
 go 1.20
 
-require github.com/google/uuid v1.3.0
+require (
+	github.com/devfullcycle/gointensivo2 v0.0.0-20230307010432-2d114aeaa2da
+	github.com/mattn/go-sqlite3 v1.14.16
+	github.com/stretchr/testify v1.8.2
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/testify v1.8.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/entity/order_test.go
+++ internal/entity/order_test.go
@@ -1,12 +1,34 @@
 package entity
 
 import (
-	"github.com/tstretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestIfIGetAnErrorIfIDIsBlank(t *testing.T) {
+func Test_If_It_Gets_An_Error_If_ID_Is_Blank(t *testing.T) {
 	order := Order{}
-	err := order.Validate()
 	assert.Error(t, order.Validate(), "invalid id")
+}
+
+func Test_If_It_Gets_An_Error_If_Price_Is_Blank(t *testing.T) {
+	order := Order{ID: "123"}
+	assert.Error(t, order.Validate(), "invalid price")
+}
+
+func Test_If_It_Gets_An_Error_If_Tax_Is_Blank(t *testing.T) {
+	order := Order{ID: "123", Price: 10.0}
+	assert.Error(t, order.Validate(), "invalid tax")
+}
+
+func Test_WithAllValidParams(t *testing.T) {
+	order := Order{ID: "123", Price: 10.0, Tax: 1.0}
+	assert.NoError(t, order.Validate())
+	assert.Equal(t, 10.0, order.Price)
+	assert.Equal(t, 1.0, order.Tax)
+	err := order.CalculateFinalPrice()
+	if err != nil {
+		return
+	}
+	assert.Equal(t, 11.0, order.FinalPrice)
 }
